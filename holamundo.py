d8d15d8860a770ec7f6fc5d799f43acc099fd1dd
Hec98 <hector982015@gmail.com>
2021-08-03 22:51:28 -0500

    HTTP methods

--- holamundo.py
+++ holamundo.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello World'
 
-@app.route('/post/<int:post_id>')
+# GET, POST, PUT, PATCH, DELETE
+@app.route('/post/<int:post_id>', methods = ['GET', 'POST'])
 def lala(post_id):
     # return 'The post id is ' + str(post_id)
-    return f'The post id is {post_id}'
+    if request.method == 'GET': return f'The post id is {post_id}'
+    else: return f'This is another method and not GET'
