aab1fb8376e7c849c853b7e9d0122d0b754049ae
Hec98 <hector982015@gmail.com>
2021-08-04 22:27:58 -0500

    Aborting petitions

--- helloWorld.py
+++ helloWorld.py
@@ -1,17 +1,16 @@
-from flask import Flask, request, url_for, redirect
+from flask import Flask, request, url_for, redirect, abort
 app = Flask(__name__)
 
 @app.route('/')
 
-def index():
-    return 'Hello World'
+def index(): return 'Hello World'
 
 # GET, POST, PUT, PATCH, DELETE
-@app.route('/post/<int:post_id>', methods = ['GET', 'POST'])
+@app.route('/post/<int:post_id>', methods = ['GET'])
 def lala(post_id):
     # return 'The post id is ' + str(post_id)
     if request.method == 'GET': return f'The post id is {post_id}'
-    else: return f'This is another method and not GET'
+    else: abort(403, 'This is another method and not GET')
 
 @app.route('/form', methods = ['POST'])
 def form():
