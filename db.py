8a9528b2000a595c08ad84337d4f4dd029a7e806
Hec98 <hector982015@gmail.com>
2021-08-06 20:29:18 -0500

    Add insertData

--- db.py
+++ db.py
@@ -12,3 +12,10 @@ def selectDataBase():
     cursor.execute('SELECT * FROM Usuario')
     result = cursor.fetchall()
     return result
+
+def insertData(user, email, age):
+    sql = 'INSERT INTO Usuario (username, email, age) VALUES (%s, %s, %s)'
+    values = (user, email, age)
+    cursor.execute(sql, values)
+    myDB.commit()
+    return cursor.rowcount
